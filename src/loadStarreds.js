ecc4d7fe8e2faa51dd26a5bfdd9c4e0681e70a7a
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-12-07 17:46:00 -0200

    refactoring

--- src/loadStarreds.js
+++ src/loadStarreds.js
@@ -4,15 +4,17 @@ const paginatonParser = require('github-pagination-parser')
 const loadStarreds = (username, page = 1, acc = []) => {
   const params = { per_page: 100, page }
   const path = `/users/${username}/starred`;
-  return http.get(path, { params })
-    .then(({ data, headers }) => {
-      const link = paginatonParser(headers.link)
-      if (link.next !== undefined) {
-        return loadStarreds(username, link.next, [...acc, ...data])
-      }
-
-      return [...acc, ...data]
-    })
+  return http.get(path, { params }).then(response => processResponse(response, username, acc))
 };
+
+const processResponse = ({ data, headers }, username, acc) => {
+  const link = paginatonParser(headers.link)
+  const newData = [...acc, ...data]
+  if (link.next !== undefined) {
+    return loadStarreds(username, link.next, newData)
+  }
+
+  return newData
+}
 
 module.exports = loadStarreds;
