079b153fd4d2a7366eed6dccf38d345d0a9e2c7f
Vinicius Reis <luiz.vinicius73@gmail.com>
2016-12-12 14:01:46 -0200

    bugfix

--- src/loadStarreds.js
+++ src/loadStarreds.js
@@ -4,11 +4,12 @@ const paginatonParser = require('github-pagination-parser')
 const loadStarreds = (username, page = 1, acc = []) => {
   const params = { per_page: 100, page }
   const path = `/users/${username}/starred`
-  return http.get(path, { params }).then(response => processResponse(response, username, acc))
+  return http.get(path, { params })
+    .then(response => processResponse(response, username, acc))
 }
 
 const processResponse = ({ data, headers }, username, acc) => {
-  const link = paginatonParser(headers.link)
+  const link = headers.link ? paginatonParser(headers.link) : {}
   const newData = [...acc, ...data]
   if (link.next !== undefined) {
     return loadStarreds(username, link.next, newData)
